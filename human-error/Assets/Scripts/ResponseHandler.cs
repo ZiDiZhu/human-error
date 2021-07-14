d1a1bfbfd63e7f08ff6831bfffd3c6c7d11ed735
Nebularswamp <grosswamp@gmail.com>
2021-07-14 02:54:26 -0400

    Working on a responses part of the dialogue system
    #2
    
    I'm imagining that the typewriter effect can be altered:
    - Faster or slower paced
    - like a super slow sentence appearing letter by letter can set an uncanny mood
    - Randomized text colors (different colors in text with different significance?)
    
    - also maybe add text animated effects?
    
    ##responses
    Can be used to trigger events /cutscenes

---
+++ human-error/Assets/Scripts/ResponseHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponseHandler : MonoBehaviour
+{
+    [SerializeField] private RectTransform responseBox;
+    [SerializeField] private RectTransform responseButtonTemplate;
+    [SerializeField] private RectTransform responseContainer;
+
+    public void ShowResponses (Response[] responses)
+    {
+
+    }
+}
