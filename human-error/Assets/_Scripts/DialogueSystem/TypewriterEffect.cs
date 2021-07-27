075dc2aa29e44cdf66f0a7fbf54131f3d5435152
Nebularswamp <grosswamp@gmail.com>
2021-07-27 17:52:33 -0400

    Added typewriter SFX
    Currently only functional in the cageroom1 scene
    
    #10

--- human-error/Assets/_Scripts/DialogueSystem/TypewriterEffect.cs
+++ human-error/Assets/_Scripts/DialogueSystem/TypewriterEffect.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Audio;
 
 //this code was taken from Semag Games' tutorial
 
@@ -10,6 +11,8 @@ public class TypewriterEffect : MonoBehaviour
 
     //note: SerializeField is used when you want to see a private  variable in the editor
     [SerializeField] private float typeSpeed = 50f;
+    public AudioSource typeSound;
+    public bool isPlayingSound = false;
 
     public Coroutine Run(string textToType, TMP_Text textLabel)
     {
@@ -19,15 +22,16 @@ public class TypewriterEffect : MonoBehaviour
     private IEnumerator TypeText(string textToType, TMP_Text textLabel)
     {
 
-      textLabel.text = string.Empty;
+          textLabel.text = string.Empty;
 
-      //yield return new WaitForSeconds(1);
+          //yield return new WaitForSeconds(1);
 
-      float t = 0;
-      int charIndex = 0;
+          float t = 0;
+          int charIndex = 0;
 
       while (charIndex < textToType.Length)
       {
+            PlayTypeSound();
           t += Time.deltaTime * typeSpeed;
           charIndex = Mathf.FloorToInt(t);
           charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
@@ -37,6 +41,17 @@ public class TypewriterEffect : MonoBehaviour
           yield return null;
       }
 
-      textLabel.text = textToType;
+        typeSound?.Stop();
+        isPlayingSound = false;
+        textLabel.text = textToType;
+    }
+
+    void PlayTypeSound()
+    {
+        if (!isPlayingSound)
+        {
+            typeSound?.Play();
+            isPlayingSound = true;
+        }
     }
 }
