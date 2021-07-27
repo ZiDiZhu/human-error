df7d87e5d175b3410964db13c77437db7c86779c
Nebularswamp <grosswamp@gmail.com>
2021-07-27 01:32:32 -0400

    addedd typewriter effect to 3d text

--- human-error/Assets/_Scripts/UnlockNext.cs
+++ human-error/Assets/_Scripts/UnlockNext.cs
@@ -1,20 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UnlockNext : MonoBehaviour
 {
     //used to unlock next item once this is triggered i guess
 
-    public GameObject nextToUnlock;
+    public GameObject[] nextToUnlock;
+    private TypewriterEffect typewriterEffect;
+
+    public string txt;
+    public TMP_Text label;
+
 
     private void Start()
     {
-        nextToUnlock.SetActive(false);
+        typewriterEffect = GetComponent<TypewriterEffect>();
+        foreach (GameObject i in nextToUnlock)
+        {
+            i.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        nextToUnlock.SetActive(true);
+        
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (GameObject i in nextToUnlock)
+            {
+                i.SetActive(true);
+            }
+            
+
+            typewriterEffect.Run(txt, label);
+        }
     }
 }
