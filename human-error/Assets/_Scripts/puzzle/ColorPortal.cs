e4942240a45bff80d3b76e6283d30a913eaf9ebc
Nebularswamp <grosswamp@gmail.com>
2021-07-30 04:36:06 -0400

    basic puzzle mechanic is set
    #35
    
    this is probably the worst way possible to code it but it's functional
    
    fuck

--- human-error/Assets/_Scripts/puzzle/ColorPortal.cs
+++ human-error/Assets/_Scripts/puzzle/ColorPortal.cs
@@ -6,23 +6,33 @@ public class ColorPortal : MonoBehaviour
 {
     public GameObject destination;
     public GameObject avatar;
+    public GameObject child;
 
+    public GameObject retryButton;
+
+    public bool isDead;
 
     private void Start()
     {
+        child = this.gameObject.transform.GetChild(0).gameObject;
+        child.SetActive(false);
+        //retryButton?.SetActive(false);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            if (destination != null)
+            if (destination != null && !destination.GetComponent<ColorPortal>().isDead)
             {
                 avatar.transform.position = destination.transform.position;
-                Destroy(this.gameObject);
+                child.SetActive(true);
+                isDead = true;
+                GetComponent<Collider2D>().enabled = false;
             }
             else
             {
                 Debug.Log("oh no u'r trapped");
+                //retryButton?.SetActive(true); //why does ?. still gives error messages 
             }
         }
     }
